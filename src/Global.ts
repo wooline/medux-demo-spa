1b4d84deeddb658d82337e3ee2826be3446994eb
wooline <wooline@qq.com>
2019-08-20 11:43:26 +0800

    fix bugs

--- src/Global.ts
+++ src/Global.ts
@@ -13,8 +13,6 @@ type EnumViewNames = typeof viewNames;
 type EnumModuleNames = typeof moduleNames;
 type LoadView = typeof loadView;
 
-export {};
-
 declare global {
   // 定义获取全局设置的函数，为了在上线后可以由运维修改，该函数的实现放在/public/index.html中，以防止被 webpack 打包
   interface InitEnv {
